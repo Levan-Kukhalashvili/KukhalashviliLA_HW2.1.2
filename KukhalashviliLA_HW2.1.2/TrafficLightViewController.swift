1498973c7a0a4819a14d7b0af7a195359a08a7bc
KukhalashviliLA <48877065+KukhalashviliLA@users.noreply.github.com>
2020-10-13 19:42:55 +0300

    Main.storyboard

--- KukhalashviliLA_HW2.1.2/TrafficLightViewController.swift
+++ KukhalashviliLA_HW2.1.2/TrafficLightViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TrafficLightViewController.swift
 //  KukhalashviliLA_HW2.1.2
 //
 //  Created by Admin on 13.10.2020.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TrafficLightViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
